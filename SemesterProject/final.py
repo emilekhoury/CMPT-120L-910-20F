011552396b4ea243bdd76bb979c3da4530b0a09c
emilekhoury <70536908+emilekhoury@users.noreply.github.com>
2020-10-28 22:49:30 -0400

    semester project commit 2

--- SemesterProject/final.py
+++ SemesterProject/final.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+# Testing
+app.run(host='localhost', port=8080, debug=True)
     
