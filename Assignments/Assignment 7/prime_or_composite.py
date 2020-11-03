67d33a5d3b776d32f21aad3fb54fa44c2937f140
emilekhoury <70536908+emilekhoury@users.noreply.github.com>
2020-11-02 22:20:45 -0500

    assignmnet 6 and 7 and final project css

--- Assignments/Assignment 7/prime_or_composite.py
+++ Assignments/Assignment 7/prime_or_composite.py
@@ -1,11 +1,19 @@
 def prime_or_composite(number):
-    """
-    - Add code in the defined function to figure out whether or not the given number is a prime number or not. 
-
-    - A prime number (or a prime) is a natural number greater than 1 that is not a product of two smaller natural numbers. A natural number greater than 1 that is not prime is called a composite number. - Wikipedia
     
-    - Take in a parameter called number and return “Prime” or “Composite”
-    """
+    #- Add code in the defined function to figure out whether or not the given number is a prime number or not. 
+
+    #- A prime number (or a prime) is a natural number greater than 1 that is not a product of two smaller natural numbers. A natural number greater than 1 that is not prime is called a composite number. - Wikipedia
+    
+    #- Take in a parameter called number and return “Prime” or “Composite”
+
+    if number<=1:
+        return "not a prime or composite number"
+    elif number>1:
+        for i in range(2, number):
+            if number%i==0:
+                return "Composite"
+        
+        return "Prime"
     pass
 
 if __name__ == "__main__":
--- SemesterProject/final.py
+++ SemesterProject/final.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/hello/')
+@app.route('/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
