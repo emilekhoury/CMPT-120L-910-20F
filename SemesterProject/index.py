200f6b527f0ad7ab7880b1a0f2831f0914b919ce
emilekhoury <70536908+emilekhoury@users.noreply.github.com>
2020-12-02 21:07:37 -0500

    404 workings

--- SemesterProject/index.py
+++ SemesterProject/index.py
@@ -5,13 +5,17 @@ app = Flask(__name__)
 @app.route('/')
 def home(): 
     app.logger.info("User connected to home route")
-    return render_template('home.html')
-
+    return render_template('home.html')gi
 
 @app.route('/about')
 def about():
     app.logger.info("User connected to about route")
     return render_template('about_me.html')
+
+@app.errorhandler(404)
+def error_page(e):
+    app.logger.info("User connected to 404")
+    return render_template("404.html"), 404
 
 # Testing
 app.run(host='localhost', port=8080, debug=True)
