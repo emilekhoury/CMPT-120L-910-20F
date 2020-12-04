c7b518dae86fdf938e2fc434b02c7722f9b89662
emilekhoury <70536908+emilekhoury@users.noreply.github.com>
2020-12-04 16:48:49 -0500

    Semester project

--- SemesterProject/index.py
+++ SemesterProject/index.py
@@ -5,12 +5,22 @@ app = Flask(__name__)
 @app.route('/')
 def home(): 
     app.logger.info("User connected to home route")
-    return render_template('home.html')gi
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
     app.logger.info("User connected to about route")
     return render_template('about_me.html')
+
+@app.route('/contact')
+def contact():
+    app.logger.info("User connected to conact me route")
+    return render_template('contact.html') 
+    
+@app.route('/person')
+def person():
+    app.logger.info("User connected to personality route")
+    return render_template('personality.html') 
 
 @app.errorhandler(404)
 def error_page(e):
